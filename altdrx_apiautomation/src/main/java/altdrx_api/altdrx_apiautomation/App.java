c5d664d9e3b017e5715c0cff76b6f59b6a7e2e49
Admin <Admin@DESKTOP-37FB485>
2024-08-14 13:22:08 +0530

    Commiting Resigstration

---
+++ altdrx_apiautomation/src/main/java/altdrx_api/altdrx_apiautomation/App.java
@@ -0,0 +1,13 @@
+package altdrx_api.altdrx_apiautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
